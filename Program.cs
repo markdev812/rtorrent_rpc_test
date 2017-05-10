e647d354a2024fb2a1f613fb7f9fb9a25afbeb01
markdev812 <mahogland@gmail.com>
2017-05-10 00:06:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rtorrent_rpc_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RTorrentRpc r = new RTorrentRpc("http://imp.seedboxes.cc/mahogland/RPC");
+            r.ListMethods();
+        }
+    }
+}
---
+++ RTorrentRPC.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+
+namespace rtorrent_rpc_test
+{
+    public class RTorrentRpc
+    {
+        private string _url;
+        public RTorrentRpc(string url)
+        {
+            _url = url;
+        }
+        public string[] ListMethods()
+        {
+            List<string> methods = new List<string>();
+
+            string xml = @"<methodCall><methodName>system.listMethods</methodName></methodCall>";
+
+            string res = Post(xml);
+
+            XElement x = XElement.Parse(res);
+            //<methodResponse><params>< param >< value >< array >< data >
+            var d = x.Element("params").Element("param").Element("value").Element("array").Element("data");
+            foreach (var n in d.Elements("value"))
+            {
+                //<value><string>methodname</string></value>
+                var m = n.Element("string").Value;
+                methods.Add(m);
+
+            }
+
+            return methods.ToArray();
+        }
+
+
+        private string Post(string xml)
+        {
+            string res = "";
+            using (WebClient wc = new WebClient())
+            {
+
+                try
+                {
+                    wc.Headers.Add(HttpRequestHeader.ContentType, "text/xml");
+                    wc.Headers.Add(HttpRequestHeader.Authorization, "Basic bWFob2dsYW5kOmxhemVyMTIz");
+                    byte[] response = wc.UploadData(_url, "POST", Encoding.ASCII.GetBytes(xml));
+                    if (response != null)
+                    {
+                        res = Encoding.ASCII.GetString(response);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+
+            }
+
+            return res;
+        }
+    }
+}
